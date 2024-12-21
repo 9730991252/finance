7756bf359f88a78f26b055f6078dda95b6e39491
9730991252 <sunilkale101@gmail.com>
2024-12-21 11:45:23 +0530

    ok

--- office/templatetags/office_tag.py
+++ office/templatetags/office_tag.py
@@ -34,3 +34,41 @@ def todays_collection():
         'todays_collection': todays_collection['credit_amount__sum'],
         'office_employee': office_employee
         }
+    
+@register.inclusion_tag('inclusion_tag/office/summary.html')
+def summary():
+    total_account_holder = Account_holder.objects.all().count()
+    total_collection = Saving_account.objects.aggregate(Sum('credit_amount'))
+    total_withdrawal = Saving_account.objects.aggregate(Sum('debit_amount'))
+    total_office_employee = Office_employee.objects.filter(status=1).count()
+    loan_amount_pending = Loan.objects.filter(loan_status=1).aggregate(Sum('loan_amount'))
+    if total_collection['credit_amount__sum'] is None:
+        total_collection['credit_amount__sum'] = 0
+    if total_withdrawal['debit_amount__sum'] is None:
+        total_withdrawal['debit_amount__sum'] = 0
+    avalable_amount = total_collection['credit_amount__sum'] - total_withdrawal['debit_amount__sum']
+    return {
+        'total_account_holder': total_account_holder,
+        'total_withdrawal': total_withdrawal['debit_amount__sum'],
+        'total_office_employee': total_office_employee,
+        'loan_amount_pending': loan_amount_pending['loan_amount__sum'],
+        'avalable_amount': avalable_amount
+    }
+@register.inclusion_tag('inclusion_tag/office/todays_withdrawal.html')
+def todays_withdrawal():
+    todays_collection = Saving_account.objects.filter(date=date.today()).aggregate(Sum('debit_amount'))
+    o = Office_employee.objects.filter(status=1)
+    office_employee = []
+    for i in o:
+        a = Saving_account.objects.filter(date=date.today(), collected_by_id=i.id).aggregate(Sum('debit_amount'))
+        a = a['debit_amount__sum']
+        if a is None:
+            a = 0
+        office_employee.append({
+            'name': i.name,
+            'debit_amount': a
+        })
+    return {
+        'todays_withdrawal': todays_collection['debit_amount__sum'],
+        'office_employee': office_employee
+        }
